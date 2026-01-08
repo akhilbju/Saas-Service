b967df41cf27b335f07c68f975f4f282571c9eac
akhilbju <abiju60@gmail.com>
2026-01-08 16:20:23 +0530

    Add foreign key and index for TaskStatusHistories to ProjectTasks

---
+++ Migrations/20260108104456_historyconnection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Saas_Auth_Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class historyconnection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskStatusHistories_TaskId",
+                table: "TaskStatusHistories",
+                column: "TaskId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskStatusHistories_ProjectTasks_TaskId",
+                table: "TaskStatusHistories",
+                column: "TaskId",
+                principalTable: "ProjectTasks",
+                principalColumn: "TaskId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskStatusHistories_ProjectTasks_TaskId",
+                table: "TaskStatusHistories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskStatusHistories_TaskId",
+                table: "TaskStatusHistories");
+        }
+    }
+}
